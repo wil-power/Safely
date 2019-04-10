bbe21edd98112631491914e16da72c64f313b2f5
oppongjr <oppong751@gmail.com>
2019-04-10 09:48:04 +0000

    Safely can now send text messages from the background

--- android/app/src/main/kotlin/shinobi/code/safely/MainActivity.kt
+++ android/app/src/main/kotlin/shinobi/code/safely/MainActivity.kt
@@ -7,12 +7,15 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.*
+import android.telephony.SmsManager
 import android.util.Log
 import io.flutter.app.FlutterActivity
 import com.github.nisrulz.sensey.Sensey
 import com.github.nisrulz.sensey.ShakeDetector
+import com.github.nisrulz.sensey.TouchTypeDetector
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
+import java.lang.Exception
 
 
 class MainActivity : FlutterActivity() {
@@ -43,7 +46,7 @@ class TheService : Service() {
 
     override fun onCreate() {
         super.onCreate()
-        Sensey.getInstance().init(applicationContext)
+//        Sensey.getInstance().init(applicationContext)
         val manager = getSystemService(Context.POWER_SERVICE) as PowerManager
         wakeLock = manager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Sensor:WakeLogTag")
         val screenStateFilter = IntentFilter()
@@ -58,13 +61,55 @@ class TheService : Service() {
     val shakeListener = object : ShakeDetector.ShakeListener {
         override fun onShakeDetected() {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-                vibrator.vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE))
+                vibrator.vibrate(
+                    VibrationEffect.createOneShot(
+                        1000,
+                        VibrationEffect.DEFAULT_AMPLITUDE
+                    )
+                )
             else
                 vibrator.vibrate(1000)
         }
 
         override fun onShakeStopped() {
+            Log.e(
+                "SHAKESTOPPED::::",
+                "Shaking has stopped. I think we can all agree that Sensey rocks!"
+            )
+            sendTextMessage()
+//            Sensey.getInstance()
+//                .startTouchTypeDetection(applicationContext, threeFingerSingleTapListener)
+//            Log.e("DETECTION::::", "Three fingers detection started!")
+        }
+    }
 
+    val threeFingerSingleTapListener = object : TouchTypeDetector.TouchTypListener {
+        override fun onSwipe(p0: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onSingleTap() {
+            Log.e("SINGLE_TAP:::", "Single tap detected!")
+        }
+
+        override fun onScroll(p0: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onLongPress() {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onTwoFingerSingleTap() {
+            Log.e("TWOFINGERS_TAP:::", "Two fingers single tap detected!")
+        }
+
+        override fun onDoubleTap() {
+            Log.e("DOUBLE_TAP", "Double tap detected!")
+        }
+
+        override fun onThreeFingerSingleTap() {
+            Log.e("THREE_FINGERS:::", "Three fingers single tap detected!")
         }
     }
 
@@ -88,18 +133,34 @@ class TheService : Service() {
         return START_STICKY
     }
 
+    private fun sendTextMessage() {
+        try {
+            val smsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage(
+                "+233209050642",
+                null,
+                "This message was sent from Safely",
+                null,
+                null
+            )
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+    }
+
     inner class ScreenReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
 
             if (intent?.action == Intent.ACTION_SCREEN_ON) {
-                Sensey.getInstance().stopShakeDetection(shakeListener)
+                Sensey.getInstance().stop()
             } else if (intent?.action == Intent.ACTION_SCREEN_OFF) {
                 val runnable = Runnable {
-                    Sensey.getInstance().startShakeDetection(5f, 5000 ,shakeListener)
+                    Sensey.getInstance().startShakeDetection(30f, 5000, shakeListener)
+                  //  Sensey.getInstance()
+                    // .startTouchTypeDetection(context, threeFingerSingleTapListener)
                 }
                 Handler().postDelayed(runnable, 500)
             }
-
         }
     }
 }
