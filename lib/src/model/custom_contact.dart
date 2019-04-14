2569724b73c51f81ef4290fa0c64e09a528b258c
oppongjr <oppong751@gmail.com>
2019-04-14 23:33:11 +0000

    Error in (de)serializing phone numbers fixed

--- lib/src/model/custom_contact.dart
+++ lib/src/model/custom_contact.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 
 import 'package:contacts_service/contacts_service.dart';
 import 'package:pref_dessert/pref_dessert.dart';
@@ -13,9 +12,20 @@ class CustomContact {
 class JsonCustomContactDesSer extends DesSer<CustomContact> {
   @override
   CustomContact deserialize(String s) {
-    print("Deserialized: $s");
-    var con = Contact(givenName: s);
-    con.displayName = s;
+    var toDeserialize = s.split(",");
+    List<Item> phoneList = [];
+    var con = Contact(givenName: toDeserialize[0],);
+    con.displayName = con.givenName;
+    if (toDeserialize.length > 2){
+      for (int i = 1; i < toDeserialize.length; i++){
+        var newItem =Item(value: toDeserialize[i]);
+        phoneList.add(newItem);
+      }
+    }else {
+      phoneList.add(Item(value: toDeserialize[1]));
+    }
+
+    con.phones = phoneList;
     return CustomContact(contact: con);
   }
 
@@ -25,8 +35,15 @@ class JsonCustomContactDesSer extends DesSer<CustomContact> {
 
   @override
   String serialize(CustomContact t) {
-    print("Serialize: ${t.contact.displayName}");
-    return "${t.contact.displayName}";
+    var phoneString ="";
+
+    if(t.contact.phones.toList().length > 1) {
+      phoneString = t.contact.phones.toList().map((item) => item.value).join(",");
+    }else{
+      phoneString = t.contact.phones.first.value.toString();
+    }
+
+    return "${t.contact.displayName},${phoneString.split(" ").join("")}";
   }
 
 }
